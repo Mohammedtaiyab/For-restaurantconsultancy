85783422eb16a30fbd1307c5d488e71dd18302f3
Mohammedtaiyab <55629150+Mohammedtaiyab@users.noreply.github.com>
2021-01-21 22:00:39 +0530

    Contact Us

--- contact.php
+++ contact.php
@@ -36,118 +36,125 @@ $indexhead=$header->getData($x['filename']);
               <a href="mailto:hello@for-restaurantconsultancy.com"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span>hello@for-restaurantconsultancy.com</span></a>
               <p  style="color: #0b0a0a;"><img src="assets/img/whatsapp.png" style="width:6%;"><i class="fas fa-phone"style="animation: none;"></i> <span>+91 9 1052 1052 9</span></p>
               <?php echo $indexhead[0]['Paragraph']; ?>
-             
+             <div class="ul-sec">
+             <h1 style="    color: #40409e;">What Will be next Step?</h1>
+             <ul class="ul-style">
+          <li class="ul-active">1. We'll Prepare a Proposal</li>
+          <li class="ul-active">2. Let's Connect for Further Discussions</li>
+          <li class="ul-active">3. Let's Start Building</li>
+          </ul>  
+             </div>
             </div>
           </div>
           <div class="col-md-8 col-md-offset-1">
-          <form action="contact.php" method="post">
+          <div class="glass">
+  <form action="contact.php" method="post">
           <div class="row">
-          <div class="col-md-6">
-                          <div class="form-group">
+          <div class="col-md-12">
+                          <div class="form-group  bd-white">
                           <input type="text" class="form-control effect-4" name="fname" placeholder="Full Name" required>
-                        
                         </div>
                       </div>
                       <div class="col-md-6">
-                          <div class="form-group">
+                          <div class="form-group  bd-white">
                           <input type="email" class="form-control effect-4" name="email" placeholder="Email" required>
                           </div>
                       </div>
                       <div class="col-md-6">
-                          <div class="form-group">
+                          <div class="form-group  bd-white">
                           <input type="text" class="form-control effect-4" name="phone" placeholder="Phone Number" required>
                           </div>
                       </div>
                       <div class="col-md-6">
-                          <div class="form-group">
+                          <div class="form-group  bd-white">
                           <input type="text" class="form-control effect-4" name="company" placeholder="Company Name" required>
                           </div>
                       </div>
                       <div class="col-md-6">
-                          <div class="form-group">
+                          <div class="form-group  bd-white">
                           <input type="text" id="et_pb_contact_interested_in_0"  name="location" class="input form-control effect-4" placeholder="Location" required>
                           </div>
                       </div>
-                      <div class="col-md-6">
-                          <div class="form-group">
-                       
-                          </div>
-                      </div>    <div class="col-md-6">
-                          <div class="form-group">
-                          <label for="et_pb_contact_message_0"  class="et_pb_contact_form_label effect-4">Message</label>
-                <textarea  name="msg" placeholder="Tell Us About Your Goals" required></textarea>
-                          </div>
-                      </div>
-                      <div class="col-md-6">
+                      
+                      <div class="col-md-12" style="
+    margin-bottom: 9px;text-align: initial;
+">
                       <div class="row">
                  <div class="col-md-6">
-                      <div class="form-group">
+                 <div class="form-group  ">
                     <input class="form-check-input effect-4" name="service[]" type="checkbox" id="inlineCheckbox1" value="Concept Design">
-                      <label class="form-check-label" for="inlineCheckbox1">Concept Design</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox1">Concept Design</label>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox2" value="Operation Strategy">
-                      <label class="form-check-label" for="inlineCheckbox2">Operation Strategy</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox2">Operation Strategy</label>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox3" value="Training">
-                      <label class="form-check-label" for="inlineCheckbox3">Training</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox3">Training</label>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group  ">
                   <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox4" value="Restaurant Assessment">
-                      <label class="form-check-label" for="inlineCheckbox4">Restaurant Assessment</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox4">Restaurant Assessment</label>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox5" value="Menu Development Design">
-                      <label class="form-check-label" for="inlineCheckbox5">Menu Development Design</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox5">Menu Development Design</label>
                   </div>
                       </div>
                       <div class="col-md-6">
 
-                  <div class="form-group">
+                  <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox6" value="Licensing">
-                      <label class="form-check-label" for="inlineCheckbox6">Licensing</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox6">Licensing</label>
                   </div>
-                  <div class="form-group">
+                  <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox7" value="Hiring">
-                      <label class="form-check-label" for="inlineCheckbox7">Hiring</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox7">Hiring</label>
                   </div>
-                    <div class="form-group">
+                    <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox8" value="Digital Marketing">
-                      <label class="form-check-label" for="inlineCheckbox8">Digital Marketing</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox8">Digital Marketing</label>
                   </div>
-                    <div class="form-group">
+                    <div class="form-group  ">
                   <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox9" value="Business Strategy">
-                <label class="form-check-label" for="inlineCheckbox9">Business Strategy</label>
+                <label class="form-check-label mylabel" for="inlineCheckbox9">Business Strategy</label>
                   </div>
-                    <div class="form-group">
+                    <div class="form-group  ">
                     <input class="form-check-input" name="service[]" type="checkbox" id="inlineCheckbox10" value="Bar Kitchen Organization">
-                      <label class="form-check-label" for="inlineCheckbox10">Bar Kitchen Organization</label>
+                      <label class="form-check-label mylabel" for="inlineCheckbox10">Bar Kitchen Organization</label>
                   </div>
                 </div>
                       </div>
                      
                       </div>
-                  
-                      <div class="col-md-8">
-                          <div class="">
+                      <div class="col-md-12">
+                          <div class="form-group ">
+                          <label for="et_pb_contact_message_0"  class="et_pb_contact_form_label effect-4">Message</label>
+                <textarea  name="msg" placeholder="Tell Us About Your Goals" required></textarea>
+                          </div>
+                      </div>
+                      <div class="col-md-12">
+                            <div class="form-group "style="line-height: 1;">
                 
-           <p style="background:#78788c;color: #fff;">Your information is safe with us. We will never spam you!</p>
+                            <p style="white-space:normal;color:#000">Your information is safe with us. We will never spam you!</p>
                  
                           </div>
                       </div>
-                      <div class="col-md-4">
-                          <div class="form-group">
+                      <div class="col-md-12">
+                          <div class="form-group ">
+                        
                           <input type="submit" class="btn  btn-md  et_pb_button" name="contact" value="Send Message" style="width: auto;float: right;">
                       
                           </div>
                       </div>
              </div>
-          </form>
+            </form>
+         </div>
+      </div>
           </div>
-        </div>
-        <br /><br />
-      
+        <br/>
+      <br/>
       </div>
 </div>
 
