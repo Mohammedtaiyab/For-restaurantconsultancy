b3a04b084aeba306161e5a29ffb3fb4ed0e941d8
Mohammedtaiyab <55629150+Mohammedtaiyab@users.noreply.github.com>
2021-11-13 20:07:02 +0530

    Update DBcontroller.php

--- database/DBcontroller.php
+++ database/DBcontroller.php
@@ -1,9 +1,7 @@
 <?php
 class DBcontroller
 {
-	
-<<<<<<< HEAD
-	// 	protected $host ='localhost';
+	//protected $host ='localhost';
 	// protected $user='root';
 	// protected $password="";
 	// protected $database="u613418722_Forrestaurant";
@@ -11,13 +9,6 @@ class DBcontroller
 	protected $user='u511712962_Forrestaurant';
 	protected $password="Forrestaurant@123";
 	protected $database="u511712962_Forrestaurant";
-=======
-		protected $host ='localhost';
-	protected $user='u511712962_Forrestaurant';
-	protected $password="Forrestaurant@123";
-	protected $database="u511712962_Forrestaurant";
-
->>>>>>> 1f28d4187baf702cce340db5611a8dcc765882dc
 	public $con=null;
 public function __construct(){
  	$this->con =mysqli_connect($this->host,$this->user,$this->password,$this->database);
